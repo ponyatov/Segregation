3c72c987d9359fc2b0be49a70c18a76ae0516ced
explorer <explorer@explorer.explorer>
2022-03-02 12:31:31 +0300

    pointless reset on connection

--- Shutdown.hpp
+++ Shutdown.hpp
@@ -7,6 +7,4 @@ void __thiscall Redirected_Shutdown(void* Unknown_Parameter, char* Reason)
 	(decltype(&Redirected_Shutdown)(Original_Shutdown_Caller_Location))(Unknown_Parameter, (char*)"Segregation");
 
 	__builtin_memset(Players_Data, 0, sizeof(Players_Data));
-
-	__builtin_memset(Players_History, 0, sizeof(Players_History));
 }
