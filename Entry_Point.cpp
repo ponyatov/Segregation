7d36cc2b0d53ee3f32f8ecb85b89d6996e82e3d9
explorer <explorer@explorer.explorer>
2022-03-04 08:35:28 +0300

    what if new round started before bullet registered?

--- Entry_Point.cpp
+++ Entry_Point.cpp
@@ -358,6 +358,8 @@ __int32 __stdcall DllMain(void* This_Module_Location, unsigned __int32 Call_Reas
 
 			using Add_Listener_Type = __int8(__thiscall*)(void* Game_Event_Manager, void* Listener, char* Event, __int8 Unknown_Parameter);
 
+			Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"round_start", 0);
+
 			Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"player_death", 0);
 
 			Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"bullet_impact", 0);
--- Event_Listener.hpp
+++ Event_Listener.hpp
@@ -10,62 +10,69 @@ void __stdcall Event_Processor(void* Event)
 
 		char* Name = Get_Name_Type((unsigned __int32)537579280)(Event);
 
-		using Identifier_To_Number_Type = __int32(__thiscall*)(void* Engine, __int32 Identifier);
-
-		using Get_Integer_Type = __int32(__thiscall*)(void* Event, char* Key, void* Unknown_Parameter);
-
-		__int32 Local_Player_Number = *(__int32*)((unsigned __int32)Local_Player + 80);
-
-		if (Name[0] == 'p')
+		if (Name[0] == 'r')
 		{
-			__int32 Victim_Number = Identifier_To_Number_Type(537020000)((void*)540435380, Get_Integer_Type(537579136)(Event, (char*)"userid", nullptr));
+			Queued_Bullets = 0;
+		}
+		else
+		{
+			using Identifier_To_Number_Type = __int32(__thiscall*)(void* Engine, __int32 Identifier);
 
-			__int32 Killer_Number = Identifier_To_Number_Type(537020000)((void*)540435380, Get_Integer_Type(537579136)(Event, (char*)"attacker", nullptr));
+			using Get_Integer_Type = __int32(__thiscall*)(void* Event, char* Key, void* Unknown_Parameter);
 
-			if (Victim_Number == Local_Player_Number)
+			__int32 Local_Player_Number = *(__int32*)((unsigned __int32)Local_Player + 80);
+
+			if (Name[0] == 'p')
 			{
-				Player_Data_Structure* Player_Data = &Players_Data[Killer_Number - 1];
+				__int32 Victim_Number = Identifier_To_Number_Type(537020000)((void*)540435380, Get_Integer_Type(537579136)(Event, (char*)"userid", nullptr));
 
-				Player_Data->Memorized = 0;
+				__int32 Killer_Number = Identifier_To_Number_Type(537020000)((void*)540435380, Get_Integer_Type(537579136)(Event, (char*)"attacker", nullptr));
 
-				if (Console_Variable_Commentator.Integer == 1)
+				if (Victim_Number == Local_Player_Number)
 				{
-					PlaySoundW((wchar_t*)Sounds_Laugh, nullptr, SND_ASYNC | SND_MEMORY);
+					Player_Data_Structure* Player_Data = &Players_Data[Killer_Number - 1];
+
+					Player_Data->Memorized = 0;
+
+					if (Console_Variable_Commentator.Integer == 1)
+					{
+						PlaySoundW((wchar_t*)Sounds_Laugh, nullptr, SND_ASYNC | SND_MEMORY);
+					}
+				}
+				else
+				{
+					if (Killer_Number == Local_Player_Number)
+					{
+						unsigned __int32 Weapon_Index = *(unsigned __int32*)((unsigned __int32)Local_Player + 2872);
+
+						void* Weapon = *(void**)((unsigned __int32)607973860 + (((Weapon_Index & 4095) - 4097) << 4));
+
+						if (Weapon != nullptr)
+						{
+							Player_Data_Structure* Player_Data = &Players_Data[Victim_Number - 1];
+
+							using Get_Primary_Ammo_Capacity_Type = __int32(__thiscall**)(void* Weapon);
+
+							Player_Data->Memorized = (*Get_Primary_Ammo_Capacity_Type(*(unsigned __int32*)Weapon + 1000))(Weapon);
+
+							Player_Data->Memorized_Y = Bruteforce_Angles[(Player_Data->Shots_Fired - 1) % 5];
+						}
+
+						if (Console_Variable_Commentator.Integer == 1)
+						{
+							PlaySoundW((wchar_t*)Sounds_Exclamation, nullptr, SND_ASYNC | SND_MEMORY);
+						}
+					}
 				}
 			}
 			else
 			{
-				if (Killer_Number == Local_Player_Number)
+				__int32 Shooter_Identifier = Identifier_To_Number_Type(537020000)((void*)540435380, Get_Integer_Type(537579136)(Event, (char*)"userid", nullptr));
+
+				if (Shooter_Identifier == Local_Player_Number)
 				{
-					unsigned __int32 Weapon_Index = *(unsigned __int32*)((unsigned __int32)Local_Player + 2872);
-
-					void* Weapon = *(void**)((unsigned __int32)607973860 + (((Weapon_Index & 4095) - 4097) << 4));
-
-					if (Weapon != nullptr)
-					{
-						Player_Data_Structure* Player_Data = &Players_Data[Victim_Number - 1];
-
-						using Get_Primary_Ammo_Capacity_Type = __int32(__thiscall**)(void* Weapon);
-
-						Player_Data->Memorized = (*Get_Primary_Ammo_Capacity_Type(*(unsigned __int32*)Weapon + 1000))(Weapon);
-
-						Player_Data->Memorized_Y = Bruteforce_Angles[(Player_Data->Shots_Fired - 1) % 5];
-					}
-
-					if (Console_Variable_Commentator.Integer == 1)
-					{
-						PlaySoundW((wchar_t*)Sounds_Exclamation, nullptr, SND_ASYNC | SND_MEMORY);
-					}
+					Queued_Bullets = 0;
 				}
-			}
-		}
-		else
-		{
-			__int32 Shooter_Identifier = Identifier_To_Number_Type(537020000)((void*)540435380, Get_Integer_Type(537579136)(Event, (char*)"userid", nullptr));
-
-			if (Shooter_Identifier == Local_Player_Number)
-			{
-				Queued_Bullets = 0;
 			}
 		}
 	}
