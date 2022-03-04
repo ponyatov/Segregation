d7c0e859fa01ebda571d59721fdb6ad0743e3956
explorer <explorer@explorer.explorer>
2022-03-04 06:49:50 +0300

    uninitialized data fix if reconnect forced by server

--- Entry_Point.cpp
+++ Entry_Point.cpp
@@ -52,6 +52,8 @@
 
 #include "Event_Listener.hpp"
 
+#include "Write_Events.hpp"
+
 #include "Shutdown.hpp"
 
 __int32 __stdcall DllMain(void* This_Module_Location, unsigned __int32 Call_Reason, void* Reserved)
@@ -359,6 +361,8 @@ __int32 __stdcall DllMain(void* This_Module_Location, unsigned __int32 Call_Reas
 			Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"player_death", 0);
 
 			Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"bullet_impact", 0);
+
+			Redirection_Manager::Redirect_Function(Original_Write_Events_Caller_Location, 2, (void*)537582208, 1, (void*)Redirected_Write_Events);
 
 			wprintf(L"[ + ] Events\n");
 
--- Shutdown.hpp
+++ Shutdown.hpp
@@ -5,6 +5,4 @@ void* Original_Shutdown_Caller_Location;
 void __thiscall Redirected_Shutdown(void* Unknown_Parameter, char* Reason)
 {
 	(decltype(&Redirected_Shutdown)(Original_Shutdown_Caller_Location))(Unknown_Parameter, (char*)"Segregation");
-
-	__builtin_memset(Players_Data, 0, sizeof(Players_Data));
 }
