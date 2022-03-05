e2f8a1ab5681b444a1fdb8bedb48ca8c4d38a5d9
explorer <explorer@explorer.explorer>
2022-03-05 14:27:02 +0300

    Commit

--- Bruteforce_Memory.hpp
+++ Bruteforce_Memory.hpp
@@ -2,11 +2,11 @@
 
 void Bruteforce_Memory_Reset()
 {
-	__int32 Entity_Number = 1;
+	__int32 Entity_Number = 0;
 
 	Traverse_Players_Data_Label:
 	{
-		Player_Data_Structure* Player_Data = &Players_Data[Entity_Number - 1];
+		Player_Data_Structure* Player_Data = &Players_Data[Entity_Number];
 
 		if (Player_Data->Memorized != 0)
 		{
@@ -15,7 +15,7 @@ void Bruteforce_Memory_Reset()
 			Player_Data->Shots_Fired = (Player_Data->Shots_Fired - 1) % 5;
 		}
 
-		if (Entity_Number <= 64)
+		if (Entity_Number < 64)
 		{
 			Entity_Number += 1;
 
