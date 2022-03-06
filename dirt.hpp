28879df327104d3171be3b2cab6cb754de961afc
explorer <explorer@explorer.explorer>
2022-03-06 07:05:54 +0300

    reducetimers

--- dirt.hpp
+++ dirt.hpp
@@ -8,6 +8,8 @@ namespace dirt
 
 	float movegrab[3];
 
+	float origingrab[3];
+
 	float origin[3];
 
 	void* org;
@@ -19,7 +21,6 @@ namespace dirt
 	void give(void* playerr, Global_Variables_Structure* nariosetnr)
 	{
 		player = playerr;
-
 		__builtin_memcpy(velocity, (void*)((unsigned __int32)player + 224), sizeof(velocity));
 
 		__builtin_memcpy(origin, (void*)((unsigned __int32)player + 668), sizeof(origin));
@@ -29,9 +30,48 @@ namespace dirt
 		grab = 1;
 	}
 
+	void CheckVelocity()
+	{
+		int i; // esi
+		float* v3; // ecx
+
+		for (i = 0; i < 3; ++i)
+		{
+			if ((*(DWORD*)((DWORD)velocity + 4 * i) & 0x7F800000) == 0x7F800000)
+			{
+				*(DWORD*)((DWORD)velocity + 4 * i) = 0;
+			}
+			if ((*(DWORD*)((DWORD)origin+ 4 * i) & 0x7F800000) == 2139095040)
+			{
+				*(DWORD*)((DWORD)origin+ 4 * i) = 0;
+			}
+			v3 = (float*)((DWORD)velocity + 4 * i);
+			if (*v3 <= *(float*)(*(unsigned __int32*)0x243E9598 + 40))
+			{
+				if (-*(float*)(*(unsigned __int32*)0x243E9598 + 40) > *v3)
+				{
+					*(float*)((DWORD)velocity + 4 * i) = -*(float*)(*(unsigned __int32*)0x243E9598 + 40);
+				}
+			}
+			else
+			{
+				*(DWORD*)((DWORD)velocity + 4 * i) = *(DWORD*)(*(unsigned __int32*)0x243E9598 + 40);
+			}
+		}
+	}
+
 	void process()
 	{
-		*((float*)global + 3) = (double)*(int*)((unsigned __int32)player + 3592) * *((float*)global + 7);
+		float stamina = *(float*)((unsigned __int32)player + 4016);
+
+		if (stamina > 0.0)
+		{
+			stamina = stamina - *((float*)global + 4) * 1000.0;
+			if (stamina < 0.0)
+				stamina = 0.0;
+		}
+
+		*((float*)global + 3) = *(int*)((unsigned __int32)player + 3592) * *((float*)global + 7);
 		*((DWORD*)global + 4) = *((DWORD*)global + 7);
 
 		*((float*)global + 4) = *(float*)((unsigned __int32)player + 3888) * *((float*)global + 4);
@@ -41,6 +81,10 @@ namespace dirt
 			- v2 * *(float*)(*(unsigned __int32*)0x243E90D0 + 40) * *((float*)global + 4) * 0.5;
 		velocity[2] = *((float*)global + 4) * *(float*)((DWORD)player + 284)
 			+ velocity[2];
+
+		*(float*)((DWORD)player + 284) = 0;
+
+		CheckVelocity();
 
 		grab = 1;
 	}
@@ -55,6 +99,10 @@ namespace dirt
 			movegrab[1] = *(float*)(*(DWORD*)((unsigned __int32)x + 4) + 68);
 			movegrab[2] = *(float*)(*(DWORD*)((unsigned __int32)x + 4) + 72);
 
+			origingrab[0] = *(float*)(*(DWORD*)((unsigned __int32)x + 8) + 668);
+			origingrab[1] = *(float*)(*(DWORD*)((unsigned __int32)x + 8) + 672);
+			origingrab[2] = *(float*)(*(DWORD*)((unsigned __int32)x + 8) + 676);
+
 			grab = 0;
 		}
 	}
@@ -63,10 +111,13 @@ namespace dirt
 	{
 		if (grab == 0)
 		{
-			wprintf(L"%f %f %f\n",
+			wprintf(L"%f %f %f ^ %f %f %f\n",
 				Absolute(velocity[0] - movegrab[0]),
-				Absolute(velocity[0] - movegrab[0]),
-				Absolute(velocity[0] - movegrab[0]));
+				Absolute(velocity[1] - movegrab[1]),
+				Absolute(velocity[2] - movegrab[2]),
+				Absolute(origingrab[0] - origin[0]),
+				Absolute(origingrab[1] - origin[1]),
+				Absolute(origingrab[2] - origin[2]));
 		}
 	}
 
