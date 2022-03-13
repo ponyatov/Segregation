7352784077784b57ce37580f5958eb2086181371
explorer <explorer@explorer.explorer>
2022-03-13 14:22:26 +0300

    readability needs significant improvement due to missing sorting

--- Entry_Point.cpp
+++ Entry_Point.cpp
@@ -12,9 +12,9 @@
 
 #include "Extended_Interface.hpp"
 
-#include "Angles_Y.hpp"
-
 #include "Frame_Stage_Notify.hpp"
+
+#include "Angles_Y.hpp"
 
 #include "Write_Events.hpp"
 
@@ -117,186 +117,200 @@ __int32 __stdcall DllMain(void* This_Module_Location, unsigned __int32 Call_Reas
 
 			FillConsoleOutputAttribute(Standard_Output_Handle, FOREGROUND_BLUE | FOREGROUND_GREEN | FOREGROUND_RED | FOREGROUND_INTENSITY | BACKGROUND_RED, Console_Screen_Buffer_Information.dwSize.X * Console_Screen_Buffer_Information.dwSize.Y, Top_Left, &Characters_Written_Count);
 
-			wprintf(L"[ + ] Remote Thread Dispatch\n");
-
-			unsigned __int8 Cheat_Flag_Bytes[4] =
-			{
-				254,
-
-				65,
-
-				44,
-
-				195
-			};
-
-			Byte_Manager::Copy_Bytes(1, (void*)538391351, sizeof(Cheat_Flag_Bytes), Cheat_Flag_Bytes);
-
-			*(__int8*)542242676 = 1;
-
 			wprintf(L"[ + ] Delimit Standard Interface\n");
+			{
+				unsigned __int8 Cheat_Flag_Bytes[4] =
+				{
+					254,
 
-			Implement_Extended_Interface();
+					65,
+
+					44,
+
+					195
+				};
+
+				Byte_Manager::Copy_Bytes(1, (void*)538391351, sizeof(Cheat_Flag_Bytes), Cheat_Flag_Bytes);
+
+				*(__int8*)542242676 = 1;
+			}
 
 			wprintf(L"[ + ] Implement Extended Interface\n");
-
-			*(void**)608149676 = (void*)Redirected_Angles_Y;
-
-			Redirection_Manager::Redirect_Function(Original_Frame_Stage_Notify_Caller_Location, 1, (void*)604538592, 1, (void*)Redirected_Frame_Stage_Notify);
-
-			Byte_Manager::Set_Bytes(1, (void*)537149598, 6, 144);
+			{
+				Implement_Extended_Interface();
+			}
 
 			wprintf(L"[ + ] Data Networking\n");
+			{
+				Redirection_Manager::Redirect_Function(Original_Frame_Stage_Notify_Caller_Location, 1, (void*)604538592, 1, (void*)Redirected_Frame_Stage_Notify);
 
-			Redirection_Manager::Redirect_Function(Original_Write_Events_Caller_Location, 2, (void*)537582208, 1, (void*)Redirected_Write_Events);
+				*(void**)608149676 = (void*)Redirected_Angles_Y;
 
-			Event_Listener_Structure* Event_Listener = (Event_Listener_Structure*)malloc(sizeof(void*));
+				Byte_Manager::Set_Bytes(1, (void*)537149598, 6, 144);
+			}
 
-			void* Event_Listener_Table = malloc(sizeof(void*) * 2);
+			wprintf(L"[ + ] Events\n");
+			{
+				Redirection_Manager::Redirect_Function(Original_Write_Events_Caller_Location, 2, (void*)537582208, 1, (void*)Redirected_Write_Events);
 
-			*(void**)Event_Listener_Table = nullptr;
+				Event_Listener_Structure* Event_Listener = (Event_Listener_Structure*)malloc(sizeof(void*));
 
-			*(void**)((unsigned __int32)Event_Listener_Table + 4) = (void*)Event_Processor;
+				void* Event_Listener_Table = malloc(sizeof(void*) * 2);
 
-			Event_Listener->Table = Event_Listener_Table;
+				*(void**)Event_Listener_Table = nullptr;
 
-			using Add_Listener_Type = __int8(__thiscall*)(void* Game_Event_Manager, void* Listener, char* Event, __int8 Unknown_Parameter);
+				*(void**)((unsigned __int32)Event_Listener_Table + 4) = (void*)Event_Processor;
 
-			Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"player_death", 0);
+				Event_Listener->Table = Event_Listener_Table;
 
-			Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"bullet_impact", 0);
+				using Add_Listener_Type = __int8(__thiscall*)(void* Game_Event_Manager, void* Listener, char* Event, __int8 Unknown_Parameter);
 
-			wprintf(L"[ + ] Event Listener\n");
+				Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"player_death", 0);
 
-			Redirection_Manager::Redirect_Function(Original_Interpolate_Caller_Location, 0, (void*)604201536, 1, (void*)Redirected_Interpolate);
+				Add_Listener_Type(537586416)((void*)540812808, Event_Listener, (char*)"bullet_impact", 0);
+			}
 
 			wprintf(L"[ + ] Interpolation\n");
+			{
+				Redirection_Manager::Redirect_Function(Original_Interpolate_Caller_Location, 0, (void*)604201536, 1, (void*)Redirected_Interpolate);
+			}
 
-			Byte_Manager::Set_Bytes(1, (void*)605800287, 1, 235);
-
-			Redirection_Manager::Redirect_Function(Original_Update_Animation_Caller_Location, 4, (void*)605800256, 1, (void*)Redirected_Update_Animation);
-
-			Byte_Manager::Set_Bytes(1, (void*)604022867, 2, 144);
-
-			Byte_Manager::Set_Bytes(1, (void*)604022906, 12, 144);
-
-			Redirection_Manager::Redirect_Function(Original_Converge_Angles_Caller_Location, 3, (void*)604019936, 1, (void*)Redirected_Converge_Angles);
 
 			wprintf(L"[ + ] Animations\n");
+			{
+				Redirection_Manager::Redirect_Function(Original_Update_Animation_Caller_Location, 4, (void*)605800256, 1, (void*)Redirected_Update_Animation);
 
-			Redirection_Manager::Redirect_Function(1, (void*)537158848, (void*)Redirected_Run_Prediction);
+				Byte_Manager::Set_Bytes(1, (void*)605800287, 1, 235);
 
-			Byte_Manager::Set_Bytes(1, (void*)605209595, 1, 235);
+				Byte_Manager::Set_Bytes(1, (void*)604022867, 2, 144);
 
-			Redirection_Manager::Redirect_Function(Original_Run_Command_Caller_Location, 0, (void*)605207600, 1, (void*)Redirected_Run_Command);
+				Byte_Manager::Set_Bytes(1, (void*)604022906, 12, 144);
 
-			Redirection_Manager::Redirect_Function(Original_Item_Post_Frame_Caller_Location, 4, (void*)605953776, 1, (void*)Redirected_Item_Post_Frame);
+				Redirection_Manager::Redirect_Function(Original_Converge_Angles_Caller_Location, 3, (void*)604019936, 1, (void*)Redirected_Converge_Angles);
+			}
 
-			Redirection_Manager::Redirect_Function(Original_Weapon_Spread_Caller_Location, 1, (void*)605949248, 1, (void*)Redirected_Weapon_Spread);
 
 			wprintf(L"[ + ] Prediction\n");
+			{
+				Redirection_Manager::Redirect_Function(1, (void*)537158848, (void*)Redirected_Run_Prediction);
 
-			Byte_Manager::Set_Bytes(1, (void*)604993824, 3, 144);
+				Byte_Manager::Set_Bytes(1, (void*)605209595, 1, 235);
 
-			Redirection_Manager::Redirect_Function(Original_Copy_User_Command_Caller_Location, 0, (void*)604850464, 1, (void*)Redirected_Copy_User_Command);
+				Redirection_Manager::Redirect_Function(Original_Run_Command_Caller_Location, 0, (void*)605207600, 1, (void*)Redirected_Run_Command);
+
+				Redirection_Manager::Redirect_Function(Original_Item_Post_Frame_Caller_Location, 4, (void*)605953776, 1, (void*)Redirected_Item_Post_Frame);
+
+				Redirection_Manager::Redirect_Function(Original_Weapon_Spread_Caller_Location, 1, (void*)605949248, 1, (void*)Redirected_Weapon_Spread);
+			}
 
 			wprintf(L"[ + ] Commands Processing\n");
+			{
+				Byte_Manager::Set_Bytes(1, (void*)604993824, 3, 144);
 
-			Redirection_Manager::Redirect_Function(1, (void*)537142224, (void*)Redirected_Send_Move);
+				Redirection_Manager::Redirect_Function(Original_Copy_User_Command_Caller_Location, 0, (void*)604850464, 1, (void*)Redirected_Copy_User_Command);
+			}
 
 			wprintf(L"[ + ] Commands Networking\n");
-
-			unsigned __int8 Apply_View_Effects_Bytes[30] =
 			{
-				7,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				217,
-
-				31,
-
-				217,
-
-				71,
-
-				4,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				217,
-
-				95,
-
-				4,
-
-				217,
-
-				71,
-
-				8,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				144,
-
-				144
-			};
-
-			Byte_Manager::Copy_Bytes(1, (void*)604082899, sizeof(Apply_View_Effects_Bytes), Apply_View_Effects_Bytes);
+				Redirection_Manager::Redirect_Function(1, (void*)537142224, (void*)Redirected_Send_Move);
+			}
 
 			wprintf(L"[ + ] View Effects\n");
+			{
+				unsigned __int8 Apply_View_Effects_Bytes[30] =
+				{
+					7,
 
-			Redirection_Manager::Redirect_Function(1, (void*)605951488, (void*)Redirected_Draw_Crosshair);
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					217,
+
+					31,
+
+					217,
+
+					71,
+
+					4,
+
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					217,
+
+					95,
+
+					4,
+
+					217,
+
+					71,
+
+					8,
+
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					144,
+
+					144
+				};
+
+				Byte_Manager::Copy_Bytes(1, (void*)604082899, sizeof(Apply_View_Effects_Bytes), Apply_View_Effects_Bytes);
+			}
 
 			wprintf(L"[ + ] Crosshair\n");
-
-			Redirection_Manager::Redirect_Function(Original_Is_Error_Material_Caller_Location, 2, (void*)((unsigned __int32)GetModuleHandleW(L"MaterialSystem.dll") + 20432), 1, (void*)Redirected_Is_Error_Material);
-
-			Byte_Manager::Set_Bytes(1, (void*)605809168, 1, 195);
-
-			unsigned __int8 Smoke_Bytes[3] =
 			{
-				194,
+				Redirection_Manager::Redirect_Function(1, (void*)605951488, (void*)Redirected_Draw_Crosshair);
+			}
 
-				8,
-
-				0
-			};
-
-			Byte_Manager::Copy_Bytes(1, (void*)605610608, sizeof(Smoke_Bytes), Smoke_Bytes);
 
 			wprintf(L"[ + ] Materials\n");
+			{
+				Redirection_Manager::Redirect_Function(Original_Is_Error_Material_Caller_Location, 2, (void*)((unsigned __int32)GetModuleHandleW(L"MaterialSystem.dll") + 20432), 1, (void*)Redirected_Is_Error_Material);
 
-			Redirection_Manager::Redirect_Function(Original_Shutdown_Caller_Location, 0, (void*)537926128, 1, (void*)Redirected_Shutdown);
+				Byte_Manager::Set_Bytes(1, (void*)605809168, 1, 195);
+
+				unsigned __int8 Smoke_Bytes[3] =
+				{
+					194,
+
+					8,
+
+					0
+				};
+
+				Byte_Manager::Copy_Bytes(1, (void*)605610608, sizeof(Smoke_Bytes), Smoke_Bytes);
+			}
 
 			wprintf(L"[ + ] Shutdown\n");
+			{
+				Redirection_Manager::Redirect_Function(Original_Shutdown_Caller_Location, 0, (void*)537926128, 1, (void*)Redirected_Shutdown);
+			}
 		}
 	}
 
