c6b32308f6f4598699f3eefa2a7b72aa0b3726ef
explorer <explorer@explorer.explorer>
2022-03-14 20:05:56 +0300

    Commit

--- Event_Listener.hpp
+++ Event_Listener.hpp
@@ -37,21 +37,27 @@ void __stdcall Event_Processor(void* Event)
 			{
 				if (Killer_Number == Local_Player_Number)
 				{
-					unsigned __int32 Weapon_Index = *(unsigned __int32*)((unsigned __int32)Local_Player + 2872);
-
-					void* Weapon = *(void**)((unsigned __int32)607973860 + (((Weapon_Index & 4095) - 4097) << 4));
-
-					if (Weapon != nullptr)
+					if (Console_Variable_Bruteforce.Integer == 1)
 					{
-						Player_Data_Structure* Player_Data = &Players_Data[Victim_Number - 1];
+						unsigned __int32 Weapon_Index = *(unsigned __int32*)((unsigned __int32)Local_Player + 2872);
 
-						using Get_Primary_Ammo_Capacity_Type = __int32(__thiscall**)(void* Weapon);
+						void* Weapon = *(void**)((unsigned __int32)607973860 + (((Weapon_Index & 4095) - 4097) << 4));
 
-						Player_Data->Memorized = (*Get_Primary_Ammo_Capacity_Type(*(unsigned __int32*)Weapon + 1000))(Weapon);
+						if (Weapon != nullptr)
+						{
+							Player_Data_Structure* Player_Data = &Players_Data[Victim_Number - 1];
 
-						Player_Data->Tolerance = 0;
+							if (Player_Data->Priority != -2)
+							{
+								using Get_Primary_Ammo_Capacity_Type = __int32(__thiscall**)(void* Weapon);
 
-						Player_Data->Memorized_Y = Bruteforce_Angles[(Player_Data->Shots_Fired - 1) % (sizeof(Bruteforce_Angles) / sizeof(float))];
+								Player_Data->Memorized = (*Get_Primary_Ammo_Capacity_Type(*(unsigned __int32*)Weapon + 1000))(Weapon);
+
+								Player_Data->Tolerance = 0;
+
+								Player_Data->Memorized_Y = Bruteforce_Angles[(Player_Data->Shots_Fired - 1) % (sizeof(Bruteforce_Angles) / sizeof(float))];
+							}
+						}
 					}
 
 					if (Console_Variable_Commentator.Integer == 1)
